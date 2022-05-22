0b8b11e20ec4fad5b37d11957fe7c010d6ce327a
Phattarapol L <phatto2543@gmail.com>
2022-05-22 21:00:30 +0700

    Landing page layout

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -1,10 +1,14 @@
 import styles from "./Card.module.css";
 
-export default function Card() {
+export default function Card({ index }) {
   return (
-    <div className={styles.container}>
-      <div>random pic maybe</div>
-      <div>detail</div>
+    <div className={styles.container} style={{ "--delay": index + 1 + "s" }}>
+      <img src="https://picsum.photos/96" alt="" className={styles.image} />
+      <div className={styles.infos}>
+        <div>some title</div>
+        <div>some detail</div>
+        <div>see more</div>
+      </div>
     </div>
   );
 }
--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,28 +1,9 @@
-import { useState, useEffect } from "react";
+import styles from "./Nav.module.css";
 
 export default function Nav() {
-  const [position, setPosition] = useState("0 0");
-
-  useEffect(() => {
-    const routine_id = setInterval(() => {
-      const x = Math.floor(Math.random() * 60) + 20;
-      const y = Math.floor(Math.random() * 60) + 20;
-
-      setPosition(x.toString() + "% " + y.toString() + "%");
-    }, 1000);
-
-    return () => {
-      clearInterval(routine_id);
-    };
-  }, []);
-
   return (
-    <nav>
-      <div className="Logo" style={{ "--random-position": position }}>
-        <div className="Shape" />
-        <span>RA DOM TOON</span>
-      </div>
-      <div>SOME buttons</div>
-    </nav>
+    <div className={styles.container}>
+      <span className={styles.header}>RA DOM TOON</span>
+    </div>
   );
 }

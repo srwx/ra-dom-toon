b6580e55606b7abc68bffc9da3dc6585287c3422
Phattarapol L <63938605+PhattOZ@users.noreply.github.com>
2022-05-25 21:21:03 +0700

    Home page layout, Add campaign form (#2)

--- client/src/components/Body/Body.js
+++ client/src/components/Body/Body.js
@@ -1,17 +1,37 @@
 import Card from "components/Card";
-import style from "./Body.module.css";
+import Popup from "components/Popup";
+import { useState } from "react";
+import styles from "./Body.module.css";
 
 const cards = ["card1", "card2", "card3"];
 
 export default function Body() {
+  const [modalActive, setModalActive] = useState(false);
+
+  const toggleAddEventPopup = () => {
+    setModalActive(!modalActive);
+  };
+
   return (
-    <div className={style.container}>
-      <h2 className={style.header}>Trending in RA-DOM-TOON</h2>
-      <div className={style.cards_container}>
+    <div className={styles.container}>
+      <div className={styles.top_bar}>
+        <div className={styles.header}>
+          <div className={styles.shape} />
+          <h2>Trending in RA DOM TOON</h2>
+        </div>
+
+        <div className={styles.button} onClick={toggleAddEventPopup}>
+          Add event
+        </div>
+      </div>
+
+      <div className={styles.cards_container}>
         {cards.map((card, index) => (
           <Card key={index} index={index} />
         ))}
       </div>
+
+      {modalActive && <Popup closePopup={toggleAddEventPopup} />}
     </div>
   );
 }

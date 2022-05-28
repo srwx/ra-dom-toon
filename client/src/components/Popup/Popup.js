a5cffc0f8b7d0d0becdcf920fbcd5cddb8146837
Sorawong L <Sorawong2543@gmail.com>
2022-05-28 15:56:21 +0700

    feat: create campaign feature (#8)

--- client/src/components/Popup/Popup.js
+++ client/src/components/Popup/Popup.js
@@ -2,6 +2,8 @@ import { useEffect, useState, useRef } from "react";
 import { createPortal } from "react-dom";
 import Input from "components/Input";
 import styles from "./Popup.module.css";
+import factoryInstance from "utils/factoryInstance";
+import web3 from "utils/web3";
 
 export default function Popup({ closePopup }) {
   const [formData, setFormData] = useState({});
@@ -20,6 +22,20 @@ export default function Popup({ closePopup }) {
     newForm[field] = value;
 
     setFormData(newForm);
+  };
+
+  const handleSubmit = async () => {
+    const userAccount = await web3.eth.getAccounts();
+    const deployedCampaign = await factoryInstance.methods
+      .createCampaign(
+        formData.name,
+        web3.utils.toWei(formData.goal, "ether") /* Required balance */,
+        web3.utils.toWei(formData.min, "ether") /* Required cost */
+      )
+      .send({
+        from: userAccount[0],
+      });
+    console.log(deployedCampaign);
   };
 
   const handleClose = (e) => {
@@ -56,19 +72,8 @@ export default function Popup({ closePopup }) {
           field="deadline"
           syncFunction={handleInput}
         />
-        <Input
-          title="Recipient Address"
-          type="text"
-          field="recipientAddress"
-          syncFunction={handleInput}
-        />
         <div className={styles.submit_wrapper}>
-          <div
-            className={styles.submit}
-            onClick={() => {
-              console.log(formData);
-            }}
-          >
+          <div className={styles.submit} onClick={handleSubmit}>
             submit
           </div>
         </div>

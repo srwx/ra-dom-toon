cfc459323b81048732ed38e51bac83fdae62fdd9
Phattarapol L <phatto2543@gmail.com>
2022-05-28 20:28:09 +0700

    Home page: Card status

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -2,6 +2,18 @@ import { Link } from "react-router-dom";
 import styles from "./Card.module.css";
 
 export default function Card({ index, campaign }) {
+  const isExpire = new Date().getTime() > 0;
+  const goalReached = false;
+
+  const status = () => {
+    if (goalReached) {
+      return <div className={styles.achieved}>Goal reached!</div>;
+    }
+    if (isExpire) {
+      return <div className={styles.expired}>Expired</div>;
+    }
+  };
+
   return (
     <Link to={`/` + campaign.address}>
       <div
@@ -9,11 +21,14 @@ export default function Card({ index, campaign }) {
         style={{ "--delay": (index + 1) / 2 + "s" }}
       >
         <img src="https://picsum.photos/96" alt="" className={styles.image} />
+
         <div className={styles.infos}>
           <div>{campaign.address}</div>
           <div>{campaign.name}</div>
           <div>see more</div>
         </div>
+
+        <div className={styles.status}>{status()}</div>
       </div>
     </Link>
   );

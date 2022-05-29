8665622cdb7e4c3e39d78def22157656b0943907
Sorawong L <Sorawong2543@gmail.com>
2022-05-29 14:56:48 +0700

    feat: minimum contributor

--- client/src/utils/factoryInstance.js
+++ client/src/utils/factoryInstance.js
@@ -1,7 +1,7 @@
 import web3 from "./web3";
 import deployedFactory from "../build/CampaignFactory.json";
 
-const deployedAddress = "0x550d1284A63845dfa5953D4a65171BD8B8C64906";
+const deployedAddress = "0xd827Eb57d8B53a5eE1ad8A449E481AfbaBDA893D";
 
 const factoryInstance = new web3.eth.Contract(
   deployedFactory.abi,
--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -5,16 +5,18 @@ pragma solidity ^0.8.9; // Tell version of solidity that we used to writing code
 contract CampaignFactory {
     address[] deployedCampaigns;
     string[] campaignsName;
+    uint256[] deadlines;
 
-    function createCampaign(string memory campaignName, uint requiredBalance, uint requiredCost, uint256 deadline) public returns (address) {
-        Campaign newCampaign = new Campaign(msg.sender, campaignName, requiredBalance, requiredCost, deadline);
+    function createCampaign(string memory campaignName, uint requiredBalance, uint requiredCost, uint256 deadline, uint min) public returns (address) {
+        Campaign newCampaign = new Campaign(msg.sender, campaignName, requiredBalance, requiredCost, deadline, min);
         deployedCampaigns.push(address(newCampaign));
         campaignsName.push(campaignName);
+        deadlines.push(deadline);
         return address(newCampaign);
     }
 
-    function getDeployedCampaigns() public view returns (address[] memory, string[] memory) {
-        return (deployedCampaigns, campaignsName);
+    function getDeployedCampaigns() public view returns (address[] memory, string[] memory, uint256[] memory) {
+        return (deployedCampaigns, campaignsName, deadlines);
     }
 }
 
@@ -28,11 +30,12 @@ contract Campaign {
         uint contributorsCount;
         bool complete;
         uint256 deadline;
+        uint minimumContributor;
     }
 
     Information campaign;
 
-    constructor(address creator, string memory campaignName, uint balance, uint cost, uint256 deadline) {
+    constructor(address creator, string memory campaignName, uint balance, uint cost, uint256 deadline, uint min) {
         campaign.manager = payable(creator);
         campaign.name = campaignName;
         campaign.requiredBalance = balance;
@@ -40,37 +43,54 @@ contract Campaign {
         campaign.contributorsCount = 0;
         campaign.complete = false;
         campaign.deadline = deadline;
+        campaign.minimumContributor = min; 
     }
 
-    function getCampaign() public view returns (address, uint, string memory, uint, uint, uint, bool, uint256) {
+    function getCampaign() public view returns (address, uint, string memory, uint, uint, uint, bool, uint256, uint) {
         Information storage c = campaign;
-        return (c.manager, address(this).balance, c.name, c.requiredBalance, c.requiredCost, c.contributorsCount, c.complete, c.deadline);
+        return (c.manager, address(this).balance, c.name, c.requiredBalance, c.requiredCost, c.contributorsCount, c.complete, c.deadline, c.contributors[msg.sender]);
     }
 
-    function contribute(uint donate) public payable {
-        require(!campaign.complete);
+    function contribute() public payable {
+        /* 
+        ** Case1 : Not require contributor number
+        ** Case2 : Required contributor number
+        */
+
+        require(!campaign.complete);    
         
-        if(campaign.requiredCost == 0) {
-            campaign.contributors[msg.sender] = donate;
+        /* Donate session */
+        if(campaign.minimumContributor == 0) {
+            /* Case1 */
+            campaign.contributors[msg.sender] = msg.value;
             campaign.contributorsCount++;
         } else {
+            /* Case2 */
             require(msg.value >= campaign.requiredCost);
-            campaign.contributors[msg.sender] = donate;
+            campaign.contributors[msg.sender] = msg.value;
             campaign.contributorsCount++;
         }
 
+        /* Transfer session */
         if(address(this).balance >= campaign.requiredBalance) {
-            campaign.manager.transfer(address(this).balance);
-            campaign.complete = true;
-        }
-    }
-
-    function getContributorDonate() public view returns (uint) {
-        return campaign.contributors[msg.sender];
+            if(campaign.minimumContributor == 0) {
+                /* Case1 */
+                campaign.manager.transfer(address(this).balance);
+                campaign.complete = true;
+            } else if(campaign.contributorsCount == campaign.minimumContributor) {
+                /* Case2 */
+                campaign.manager.transfer(address(this).balance);
+                campaign.complete = true;
+            }
+        }    
     }
 
     function claim() public payable {
         require(block.timestamp >= campaign.deadline);
         payable(msg.sender).transfer(campaign.contributors[msg.sender]);
+    }
+
+    function getIsComplete() public view returns (bool) {
+        return campaign.complete;
     }
 }
